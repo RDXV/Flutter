3f22caacc33227a638c7073fcbe311790ec15130
Varun Arora <varunarora1408@gmail.com>
2020-08-04 16:24:13 +0530

    Learning navigation through pages in flutter

---
+++ Navigation-Flutter-Demo/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screen1.dart';
+import 'screen0.dart';
+import 'screen2.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // home: Screen0(),
+      // OR
+      initialRoute: '/',
+      // Only one can exist.If both exist, the app will crash otherwise
+      routes: {
+        '/': (context) => Screen0(),
+        '/first': (context) => Screen1(),
+        '/second': (context) => Screen2(),
+      },
+    );
+  }
+}
